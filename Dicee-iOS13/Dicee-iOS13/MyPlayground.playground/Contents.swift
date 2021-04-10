79eb2e75bda033f017543522f0a18dc70112e853
ibqaddoa <62803973+ibqaddoa@users.noreply.github.com>
2021-04-10 02:33:49 +0200

    Add files via upload

---
+++ Dicee-iOS13/Dicee-iOS13/MyPlayground.playground/Contents.swift
@@ -0,0 +1,5 @@
+var number = [45, 73, 195, 53]
+
+var computedNumber = [ [number[0]*number[1], number[1]*number[2], number[2]*number[3]] ]
+
+print(computedNumber)
---
+++ Dicee-iOS13/Dicee-iOS13/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Dicee-iOS13
+//
+//  Created by Angela Yu on 11/06/2019.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var diceViewImage1: UIImageView!
+    @IBOutlet weak var diceViewImage2: UIImageView!
+
+
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        let diceArray = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix") ]
+        
+        diceViewImage1.image = diceArray.randomElement()
+        diceViewImage2.image = diceArray.randomElement()
+        
+        
+    }
+    
+}
+
